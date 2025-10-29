78409db2420a6bca5aaaba5b701fb10efada67b7
Arellano <arellano9627@gmail.com>
2025-10-29 11:26:16 -0600

    se agregó mensaje Con retardo, a tiempo fuera de horario

--- src/App.vue
+++ src/App.vue
@@ -175,7 +175,8 @@ export default {
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                       <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                     </svg>
-                    <p class='pl-4'>Horario: ${this.alumno.hora_inicio} - ${this.alumno.hora_fin}</p>
+                    <!-- <p class='pl-4'>Horario: ${this.alumno.hora_inicio} - ${this.alumno.hora_fin}</p> -->
+                    <p class='pl-4'>Horario: ${this.alumno.mensaje_llegada}</p>
                   </div>
 
                   <div class='text-center py-4 font-semibold text-blue-500 text-[1.6rem]'>
